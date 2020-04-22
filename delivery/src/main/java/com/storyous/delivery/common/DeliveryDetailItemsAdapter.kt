0c53899bd2f8f0d3aef54acf2b23f39bd751c579
Vašek Strnad <vaekwww@gmail.com>
2020-04-22 13:22:35 +0200

    simplify square views with price and count

--- delivery/src/main/java/com/storyous/delivery/common/DeliveryDetailItemsAdapter.kt
+++ delivery/src/main/java/com/storyous/delivery/common/DeliveryDetailItemsAdapter.kt
@@ -3,15 +3,15 @@ package com.storyous.delivery.common
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.storyous.commonutils.recyclerView.ItemsAdapter
 import com.storyous.commonutils.recyclerView.getString
 import com.storyous.commonutils.show
 import com.storyous.delivery.common.api.model.DeliveryAddition
 import com.storyous.delivery.common.api.model.DeliveryItem
+import kotlinx.android.synthetic.main.list_item_delivery_detail.view.*
 import kotlinx.android.synthetic.main.payment_item_additions_subitem.view.*
+import kotlinx.android.synthetic.main.typed_value_layout.view.*
 
 class DeliveryDetailItemsAdapter :
     RecyclerView.Adapter<DeliveryDetailItemsAdapter.ItemDetailViewHolder>(),
@@ -41,12 +41,12 @@ class DeliveryDetailItemsAdapter :
     class ItemDetailViewHolder(
         itemView: View
     ) : RecyclerView.ViewHolder(itemView) {
-        private val count = itemView.findViewById<TextView>(R.id.count_value)
-        private val measure = itemView.findViewById<TextView>(R.id.count_measure)
-        private val title = itemView.findViewById<TextView>(R.id.item_title)
-        private val price = itemView.findViewById<TextView>(R.id.price_value)
-        private val currency = itemView.findViewById<TextView>(R.id.price_currency)
-        private val subitems = itemView.findViewById<LinearLayout>(R.id.subitems)
+        private val count = itemView.count_square.value
+        private val measure = itemView.count_square.type
+        private val title = itemView.item_title
+        private val price = itemView.price_square.value
+        private val currency = itemView.price_square.type
+        private val subitems = itemView.subitems
 
         fun bind(item: DeliveryItem) {
             count.text = DeliveryConfiguration.formatter.formatCount(item.count)
