f140b4efc656c67ab8cd15fbab363694425d8437
Vašek Strnad <vaekwww@gmail.com>
2021-11-22 12:14:25 +0100

    use databinding instead of android synthetic in DeliveryDetailItemsAdapter

--- delivery/src/main/java/com/storyous/delivery/common/DeliveryDetailItemsAdapter.kt
+++ delivery/src/main/java/com/storyous/delivery/common/DeliveryDetailItemsAdapter.kt
@@ -1,7 +1,6 @@
 package com.storyous.delivery.common
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.storyous.commonutils.recyclerView.ItemsAdapter
@@ -9,9 +8,8 @@ import com.storyous.commonutils.recyclerView.getString
 import com.storyous.commonutils.show
 import com.storyous.delivery.common.api.DeliveryAddition
 import com.storyous.delivery.common.api.DeliveryItem
-import kotlinx.android.synthetic.main.list_item_delivery_detail.view.*
-import kotlinx.android.synthetic.main.list_item_delivery_addition.view.*
-import kotlinx.android.synthetic.main.typed_value_layout.view.*
+import com.storyous.delivery.common.databinding.ListItemDeliveryAdditionBinding
+import com.storyous.delivery.common.databinding.ListItemDeliveryDetailBinding
 import java.math.BigDecimal
 
 class DeliveryDetailItemsAdapter :
@@ -26,8 +24,11 @@ class DeliveryDetailItemsAdapter :
 
     override fun onCreateViewHolder(parent: ViewGroup, itemType: Int): ItemDetailViewHolder {
         return ItemDetailViewHolder(
-            LayoutInflater.from(parent.context)
-                .inflate(R.layout.list_item_delivery_detail, parent, false)
+            ListItemDeliveryDetailBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
         )
     }
 
@@ -40,14 +41,14 @@ class DeliveryDetailItemsAdapter :
     }
 
     class ItemDetailViewHolder(
-        itemView: View
-    ) : RecyclerView.ViewHolder(itemView) {
-        private val count = itemView.count_square.value
-        private val measure = itemView.count_square.type
-        private val title = itemView.item_title
-        private val price = itemView.price_square.value
-        private val currency = itemView.price_square.type
-        private val subitems = itemView.subitems
+        binding: ListItemDeliveryDetailBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        private val count = binding.countSquare.value
+        private val measure = binding.countSquare.type
+        private val title = binding.itemTitle
+        private val price = binding.priceSquare.value
+        private val currency = binding.priceSquare.type
+        private val subitems = binding.subitems
 
         fun bind(item: DeliveryItem) {
             count.text = DeliveryConfiguration.formatter.formatCount(item.count)
@@ -64,27 +65,26 @@ class DeliveryDetailItemsAdapter :
             val inflater = LayoutInflater.from(itemView.context)
             subitems.removeAllViews()
             item.additions?.forEach {
-                val view =
-                    inflater.inflate(R.layout.list_item_delivery_addition, subitems, false)
-                bindSubItem(view, item, it)
-                subitems.addView(view)
+                val binding = ListItemDeliveryAdditionBinding.inflate(inflater, subitems, false)
+                bindSubItem(binding, item, it)
+                subitems.addView(binding.root)
             }
         }
 
         private fun bindSubItem(
-            subItemView: View,
+            additionBinding: ListItemDeliveryAdditionBinding,
             mainItem: DeliveryItem,
             item: DeliveryAddition
         ) {
-            subItemView.sign.text = if (item.countPerMainItem >= 0) "\uff0b " else "\u2014 "
-            subItemView.title.text = item.title.let {
+            additionBinding.sign.text = if (item.countPerMainItem >= 0) "\uff0b " else "\u2014 "
+            additionBinding.title.text = item.title.let {
                 if (item.countPerMainItem == 1) it else "${item.countPerMainItem} $it"
             }
 
             val formattedPriceWithoutCurrency = DeliveryConfiguration.formatter.formatPrice(
                 item.unitPriceWithVat.multiply(BigDecimal.valueOf(item.countPerMainItem * mainItem.count))
             )
-            subItemView.price.text = formattedPriceWithoutCurrency.let {
+            additionBinding.price.text = formattedPriceWithoutCurrency.let {
                 if (item.countPerMainItem < 0) "-$it" else it
             }
         }
