20a35192119927e05d64854c1b409137774661f3
ibartj <jan.bartovsky@storyous.com>
2021-12-31 11:04:48 +0100

    fix previous rebase error

--- delivery/src/main/java/com/storyous/delivery/common/DeliveryListFragment.kt
+++ delivery/src/main/java/com/storyous/delivery/common/DeliveryListFragment.kt
@@ -1,9 +1,7 @@
 package com.storyous.delivery.common
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.widget.PopupMenu
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
