928b3d1101e0a8c754866ff116cede7bc785b8ca
Vašek Strnad <vaekwww@gmail.com>
2022-04-14 13:31:38 +0200

    be able to use LabelLayout in horizontal orientation

--- design/src/main/kotlin/com/storyous/design/LabelLayout.kt
+++ design/src/main/kotlin/com/storyous/design/LabelLayout.kt
@@ -6,22 +6,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.core.content.res.use
 
 open class LabelLayout @JvmOverloads constructor(
-    context: Context, attrs: AttributeSet? = null
+    context: Context,
+    attrs: AttributeSet? = null
 ) : LinearLayoutCompat(context, attrs) {
 
     private val label: AppCompatTextView = LayoutInflater.from(context)
         .inflate(R.layout.label, this, false) as AppCompatTextView
+
+    private val labelLayoutParams: LinearLayout.LayoutParams
+        get() = if (orientation == VERTICAL) {
+            LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+        } else {
+            LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+        }
 
     init {
         context.obtainStyledAttributes(attrs, R.styleable.LabelLayout).use {
             setText(it.getString(R.styleable.LabelLayout_android_text))
             orientation = it.getInt(R.styleable.LabelLayout_android_orientation, VERTICAL)
         }
+
+        label.layoutParams = labelLayoutParams
     }
 
     fun setText(text: String?) {
@@ -32,7 +50,7 @@ open class LabelLayout @JvmOverloads constructor(
         super.addView(child, index, params)
         if (getChildAt(0) != label) {
             removeView(label)
-            super.addView(label, 0, params)
+            super.addView(label, 0, labelLayoutParams)
             baselineAlignedChildIndex = 0
         }
     }
@@ -44,8 +62,17 @@ open class LabelLayout @JvmOverloads constructor(
 }
 
 class TextInputLabelLayout @JvmOverloads constructor(
-    context: Context, attrs: AttributeSet? = null
+    context: Context,
+    attrs: AttributeSet? = null
 ) : LabelLayout(context, attrs) {
 
     val editText: EditText? get() = getChild(1)
+}
+
+class TextViewLabelLayout @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null
+) : LabelLayout(context, attrs) {
+
+    val textView: TextView? get() = getChild(1)
 }
